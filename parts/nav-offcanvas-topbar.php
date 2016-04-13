e1bc7a2b64d8285f71b89b61705684614236aad8
Jimmy Zooler <jimmy.zooler.com>
2016-04-12 21:09:56 -0400

    Finished styling header and fixed responsiveness.

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -2,10 +2,10 @@
 	 and a topbar for medium-up -->
 
 <div class="top-bar row" id="top-bar-menu">
-	<div class="top-bar-left-1 float-left">
+	<div class="top-bar-left-1 ">
             <a href="<?php echo home_url(); ?>"><img src="wp-content/themes/Sebastien_Schwab/Sebastien_logo_crop.jpg" width="266px"/></a>
 	</div>
-	<div class="top-bar-right-1 float-right show-for-medium">
+	<div class="top-bar-right-1 show-for-medium">
             <div class="nav-container">
 		<div class="top-nav right">
                     <?php joints_top_nav(); ?>	
@@ -19,10 +19,19 @@
                 <?php bloginfo('description'); ?>
             </div>
 	</div>
-	<div class="top-bar-right columns float-right show-for-small-only">
-		<ul class="menu">
-			 <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> 
-			<!--<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>-->
-		</ul>
+	<div class="top-bar-right float-right show-for-small-only">
+            <div class="mobile-container">
+                <div class="mobile-social">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                    <i class="fa fa-youtube" aria-hidden="true"></i>
+                </div>
+                <ul class="mobile-menu">
+                    <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> 
+                    <!--<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>-->
+                </ul>
+            </div>
+            <div class="tagline">
+                <?php bloginfo('description'); ?>
+            </div>
 	</div>
 </div>
