631a3c31df23a35b969441103e223a6101b6f7ce
Jimmy Zooler <jimmy.zooler.com>
2016-04-19 00:43:17 -0400

    Added orange bar and bottom section to lunch page. Still needs some styling and content.

--- template-lunch.php
+++ template-lunch.php
@@ -11,7 +11,7 @@ Template Name: Lunch Page
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-12 medium-12 columns" role="main">
-			    <section class="white-box about-page-section">
+			    <section class="white-box lunch-page-section">
                         
                                 <div class="row">
                                     <h2 class="section-header"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/nexa_header_leaves_left-1.jpg" max-width="80%"><?php the_field('lunch_page_header')?><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/nexa_header_leaves_right-1.jpg" max-width="80%"></h2>
@@ -31,6 +31,33 @@ Template Name: Lunch Page
                                         <?php the_field('lunch_item_4')?>
                                         <?php the_field('lunch_item_5')?>
                                         <?php the_field('lunch_item_6')?>
+                                    </div>
+                                </div>
+                                <div class="row orange-bar text-center">
+                                    <h5>Please advise you server of any food allergies.</h5>
+                                    <div class="columns medium-offset-2 medium-4 text-center">
+                                        <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/gluten_free-1.jpg">
+                                        <h5>Gluten-free options available.</h5>
+                                    </div>
+                                    <div class="columns medium-4 text-center">
+                                        <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/apple-1.jpg">
+                                        <h5>Vegetarian/Vegan options available.</h5>
+                                    </div>
+                                    <div class="column"></div>
+                                </div>
+                                <div class="row hours-location ">
+                                    <div class="columns medium-6">
+                                        <h2 class="section-header">Hours of Operation</h2>
+                                        <h2 class="no-margin-bottom">11:00am - 4:00pm</h2>
+                                        <p>Closed Wednesdays</p>
+                                        <p></p>
+                                        <h3 class="section-header">Reservations and Information</h3>
+                                        <h3 class="no-margin-bottom">613.393.5802</h3>
+                                        <p>sebastienschwabcuisine@gmail.com</p>
+                                        
+                                    </div>
+                                    <div class="columns medium-6">
+                                        <h2 class="section-header">Location</h2>
                                     </div>
                                 </div>
                                 
