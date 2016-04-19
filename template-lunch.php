bb6893aa10c07cdb7227fa4832cf31e0871abd9f
Jimmy Zooler <jimmy.zooler.com>
2016-04-19 15:21:37 -0400

    Finished lunch page. reduced inner-content width to 65rem.

--- template-lunch.php
+++ template-lunch.php
@@ -45,19 +45,22 @@ Template Name: Lunch Page
                                     </div>
                                     <div class="column"></div>
                                 </div>
-                                <div class="row hours-location ">
+                                <div class="row hours-location text-center">
                                     <div class="columns medium-6">
-                                        <h2 class="section-header">Hours of Operation</h2>
-                                        <h2 class="no-margin-bottom">11:00am - 4:00pm</h2>
-                                        <p>Closed Wednesdays</p>
+                                        <h2 class="lunch-header ">Hours of Operation</h2>
+                                        <h3 class="no-margin-bottom lunch-hours">11:00am - 4:00pm</h3>
+                                        <h4>Closed Wednesdays</h4>
                                         <p></p>
-                                        <h3 class="section-header">Reservations and Information</h3>
-                                        <h3 class="no-margin-bottom">613.393.5802</h3>
-                                        <p>sebastienschwabcuisine@gmail.com</p>
+                                        <h3 class="lunch-header  lunch-info">Reservations and Info</h3>
+                                        <h3 class="no-margin-bottom lunch-hours">613.393.5802</h3>
+                                        <h5>sebastienschwabcuisine@gmail.com</h5>
+                                        <p class="lunch-hours">We ask that groups of 10 or more contact us in advance to reserver a table.</p>
                                         
                                     </div>
-                                    <div class="columns medium-6">
-                                        <h2 class="section-header">Location</h2>
+                                    <div class="columns medium-6 lunch-location">
+                                        <h2 class="lunch-header">Location</h2>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2869.323005622836!2d-77.29101268449051!3d44.014719279110544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d635a34501720b%3A0x9ed9ec4acc8702dd!2s2274+Prince+Edward+County+Rd+1%2C+Prince+Edward%2C+ON+K0K+1G0!5e0!3m2!1sen!2sca!4v1461089580602" width="100%" height="300px" frameborder="0" style="border: 3px solid #f37021" allowfullscreen></iframe>
+                                        <p class="lunch-hours">2274 County Rd.1 (P.O. Box 300)<br>Bloomfield, ON K0K 1G0</p>
                                     </div>
                                 </div>
                                 
