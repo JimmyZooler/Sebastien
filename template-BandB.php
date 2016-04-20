e99d341f0914effd627403f3606190f78bdb3fcb
Jimmy Zooler <jimmy.zooler.com>
2016-04-20 00:52:39 -0400

    Added contact page. FInished comtact page.

--- template-BandB.php
+++ template-BandB.php
@@ -11,7 +11,49 @@ Template Name: B&B Page
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-12 medium-12 columns" role="main">
-				
+			    <section class="white-box mystic-page">
+                        
+                                <h2 class="section-header catering-header"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/nexa_header_leaves_left-1.jpg" max-width="80%">The Mystic Dandelion<img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/nexa_header_leaves_right-1.jpg" max-width="80%"></h2>
+                                <h3 class="sub-header"><?php the_field('mystic_sub_header')?></h3>
+                                <div class="row mystic-section">
+                                    <div class="columns medium-5 mystic-text">
+                                        <p><?php the_field('mystic_section_1')?></p>
+                                    </div>
+                                    <div class="columns medium-7 mystic-image">
+                                        <img src="<?php the_field('mystic_image_1')?>">
+                                    </div>
+                                </div>
+                                <div class="row mystic-section">
+                                    <div class="columns medium-5 mystic-text">
+                                        <p><?php the_field('mystic_section_2')?></p>
+                                    </div>
+                                    <div class="columns medium-7 mystic-image">
+                                        <img src="<?php the_field('mystic_image_2')?>">
+                                    </div>
+                                </div>
+                                <div class="orange-bar text-center">
+                                    <h2><?php the_field('mystic_orange_text')?></h2>
+                                </div>
+                                <div class="row mystic-rooms">
+                                    <div class="columns medium-4 mystic-room">
+                                        <h3>The Sorrel Suite</h3>
+                                        <img src="<?php the_field('mystic_room_1')?>">
+                                    </div>
+                                    <div class="columns medium-4  mystic-room">
+                                        <h3>The Burdoch Room</h3>
+                                        <img src="<?php the_field('mystic_room_2')?>">
+                                    </div>
+                                    <div class="columns medium-4  mystic-room">
+                                        <h3>The Milkweed Room</h3>
+                                        <img src="<?php the_field('mystic_room_3')?>">
+                                    </div>
+                                </div>
+                                
+                               <div id="mystic-button" class="button-container">
+                                    <a class="green-button" href="#">Find out more information</a>
+                                </div>
+                                
+                            </section>
 											
 
                     </main> <!-- end #main -->
--- template-contact.php
+++ template-contact.php
@@ -11,7 +11,18 @@ Template Name: Contact Page
 		<div id="inner-content" class="row">
 	
 		    <main id="main" class="large-12 medium-12 columns" role="main">
-				
+			    <section class="white-box mystic-page">
+                        
+                                <h2 class="section-header catering-header"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/nexa_header_leaves_left-1.jpg" max-width="80%">We’d Love to Hear from You<img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/nexa_header_leaves_right-1.jpg" max-width="80%"></h2>
+                                <h3 class="sub-header"><?php the_field('contact_sub_header')?></h3>
+                                <div class="row">
+                                    <div class="column medium-offset-3 medium-6">
+                                        <div class="contact-form-container text-left">
+                                            <?php the_field('contact_form')?>
+                                        </div>
+                                    </div>
+                                    <div class="column"></div>
+                                </div>
 											
 
                     </main> <!-- end #main -->
