bba40179c181371bf8a55dee44545af69054a0c6
Jimmy Zooler <jimmy.zooler.com>
2016-04-22 19:49:52 -0400

    Added links to footer thumbnails.

--- footer.php
+++ footer.php
@@ -3,36 +3,36 @@
                                                     <h4 id="footer-header">Friends of Chef Sebastien</h4>
                                                     <div id="footer-thumbnails" class="row column">
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/Mystic.jpg">
-                                                            <p>The Mystic Dandelion</p>
+                                                            <a href="http://www.themysticdandelion.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/Mystic.jpg">
+                                                                <p>The Mystic Dandelion</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/prince_edward.jpg">
-                                                            <p>Prince Edward County</p>
+                                                            <a href="http://www.peccounty.on.ca"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/prince_edward.jpg">
+                                                            <p>Prince Edward County</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/huff.jpg">
-                                                            <p>Huff Estates</p>
+                                                            <a href="http://www.huffestates.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/huff.jpg">
+                                                            <p>Huff Estates</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/slow_food.jpg">
-                                                            <p>Slow Food The County</p>
+                                                            <a href="http://www.slowfoodthecounty.ca"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/slow_food.jpg">
+                                                            <p>Slow Food The County</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/taste_the_county.jpg">
-                                                            <p>Taste The County</p>
+                                                            <a href="http://www.tastethecounty.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/taste_the_county.jpg">
+                                                            <p>Taste The County</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/chamber.jpg">
-                                                            <p>Chamber of Commerce</p>
+                                                            <a href="http://www.pecchamber.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/chamber.jpg">
+                                                            <p>Chamber of Commerce</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/humble.jpg">
-                                                            <p>Humble Bread</p>
+                                                            <a href="http://www.humblebread.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/humble.jpg">
+                                                            <p>Humble Bread</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/bloomfield.jpg">
-                                                            <p>Bloomfield Area Business</p>
+                                                            <a href="#"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/bloomfield.jpg">
+                                                            <p>Bloomfield Area Business</p></a>
                                                         </div>
                                                     </div>
                                                     
