719d75816125f0acc6aeb2178e90f9e8b22233d9
Jimmy Zooler <jimmy.zooler.com>
2016-04-22 20:58:32 -0400

    added footer images to theme /images folder. Added favicon to theme.

--- footer.php
+++ footer.php
@@ -3,35 +3,35 @@
                                                     <h4 id="footer-header">Friends of Chef Sebastien</h4>
                                                     <div id="footer-thumbnails" class="row column">
                                                         <div class="column small-3 footer-column">
-                                                            <a href="http://www.themysticdandelion.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/Mystic.jpg">
+                                                            <a href="http://www.themysticdandelion.com"><img src="<?php echo get_template_directory_uri(); ?>/images/Mystic.jpg">
                                                                 <p>The Mystic Dandelion</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <a href="http://www.peccounty.on.ca"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/prince_edward.jpg">
+                                                            <a href="http://www.peccounty.on.ca"><img src="<?php echo get_template_directory_uri(); ?>/images/prince_edward.jpg">
                                                             <p>Prince Edward County</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <a href="http://www.huffestates.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/huff.jpg">
+                                                            <a href="http://www.huffestates.com"><img src="<?php echo get_template_directory_uri(); ?>/images/huff.jpg">
                                                             <p>Huff Estates</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <a href="http://www.slowfoodthecounty.ca"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/slow_food.jpg">
+                                                            <a href="http://www.slowfoodthecounty.ca"><img src="<?php echo get_template_directory_uri(); ?>/images/slow_food.jpg">
                                                             <p>Slow Food The County</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <a href="http://www.tastethecounty.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/taste_the_county.jpg">
+                                                            <a href="http://www.tastethecounty.com"><img src="<?php echo get_template_directory_uri(); ?>/images/taste_the_county.jpg">
                                                             <p>Taste The County</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <a href="http://www.pecchamber.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/chamber.jpg">
+                                                            <a href="http://www.pecchamber.com"><img src="<?php echo get_template_directory_uri(); ?>/images/chamber.jpg">
                                                             <p>Chamber of Commerce</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <a href="http://www.humblebread.com"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/humble.jpg">
+                                                            <a href="http://www.humblebread.com"><img src="<?php echo get_template_directory_uri(); ?>/images/humble.jpg">
                                                             <p>Humble Bread</p></a>
                                                         </div>
                                                         <div class="column small-3 footer-column">
-                                                            <a href="#"><img src="http://localhost/sebastien/wordpress/wp-content/uploads/2016/04/bloomfield.jpg">
+                                                            <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/bloomfield.jpg">
                                                             <p>Bloomfield Area Business</p></a>
                                                         </div>
                                                     </div>
--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 		<!-- If Site Icon isn't set in customizer -->
 		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
 			<!-- Icons & Favicons -->
-			<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+			<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon_2.png">
 			<link href="<?php echo get_template_directory_uri(); ?>/assets/images/apple-icon-touch.png" rel="apple-touch-icon" />
 			<!--[if IE]>
 				<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
